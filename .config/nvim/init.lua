94bccd43c9b63c71d70856319f2fc986da7fdf34
jus <gitgoodjust4lulz@yahoo.com>
2023-04-20 06:00:05 +0200

    small improvements to nvim(some term remaps and fixes), zshrc(idk lol) and sway (layout change remap)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
+-- convert all buffers to tabs
 use = require('packer').use
 
 require('packer').startup(function()
@@ -31,7 +32,7 @@ require('packer').startup(function()
   use 'rhysd/committia.vim' -- neat commit message editor layout
 
   -- (Build system support)
-  -- use 'johnsyweb/vim-makeshift'
+  use 'johnsyweb/vim-makeshift'
 
 
   --(Package management)
@@ -47,7 +48,6 @@ require('packer').startup(function()
   use 'luochen1990/rainbow'
 
 end)
-
 --load plugins with empty setup here, other load based on preferences
 require 'nvim-autopairs'.setup{}
 
@@ -66,6 +66,7 @@ vim.o.shiftwidth = 2
 vim.o.number = true
 vim.cmd "set path=$PWD/**" 
 vim.cmd "set wildignore+=**/.git/**,**/__pycache__/**,**/venv/**,**/node_modules/**,**/dist/**,**/build/**,*.o,*.pyc,*.swp" 
+vim.cmd [[ autocmd TabEnter * if &buftype == 'terminal' | execute 'startinsert' | endif ]]
 
 --Set globals
 vim.g.python_highlight_all = 1
@@ -74,7 +75,7 @@ vim.g.mapleader = ','
 vim.g.rainbow_active = 1
 
 --Set look & feel
-vim.cmd "colorscheme gruvbox"
+vim.cmd "colorscheme molokai"
 
 --Set misc
 vim.api.nvim_exec([[
@@ -84,23 +85,28 @@ vim.cmd "command! Wq wq"
 
 --Set keymaps
 local opts = { noremap=true, silent=true }
-vim.api.nvim_set_keymap('n', '<M-l>', '<cmd>bn<cr>', opts)
-vim.api.nvim_set_keymap('n', '<M-h>', '<cmd>bp<cr>', opts)
-vim.api.nvim_set_keymap('t', '<M-h>', '<C-\\><C-n><cmd>bp<cr>', opts)
-vim.api.nvim_set_keymap('t', '<M-l>', '<C-\\><C-n><cmd>bn<cr>', opts)
+-- how do we move tabs
+vim.api.nvim_set_keymap('n', '<M-l>', '<cmd>tabnext<cr>', opts)
+vim.api.nvim_set_keymap('n', '<M-h>', '<cmd>tabprev<cr>', opts)
+vim.api.nvim_set_keymap('n', '<S-M-l>', '<cmd>+tabmove<cr>', opts)
+vim.api.nvim_set_keymap('n', '<S-M-h>', '<cmd>-tabmove<cr>', opts)
+
+vim.api.nvim_set_keymap('t', '<M-h>', '<cmd>tabprev<cr>', opts)
+vim.api.nvim_set_keymap('t', '<M-l>', '<cmd>tabnext<cr>', opts)
 vim.api.nvim_set_keymap('i', '<C-k>', '<esc>o', opts)
 vim.api.nvim_set_keymap('n', '<C-e>', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<C-h>', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<C-l>', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+vim.api.nvim_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
 
 vim.api.nvim_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 vim.api.nvim_set_keymap('n', 'vr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
 vim.api.nvim_set_keymap('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
 vim.api.nvim_set_keymap('n', 'sr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-vim.api.nvim_set_keymap('n', 'fm', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+vim.api.nvim_set_keymap('n', 'fm', '<cmd>lua vim.lsp.buf.formatting_sync(nil, 1000)<CR>', opts)
 vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
@@ -109,71 +115,71 @@ vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 local servers = { 'clangd', 'rust_analyzer', 'ltex', 'pyright', 'ocamllsp', 'gopls'}
 
 local lspconfig = require('lspconfig')
---
--- enable nvim-cmp additionsl capabilities and set up servers
+
+--enable nvim-cmp additionsl capabilities and set up servers
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-        -- on_attach = my_custom_on_attach,
-        capabilities = capabilities,
-    }
+   lspconfig[lsp].setup {
+       -- on_attach = my_custom_on_attach,
+       capabilities = capabilities,
+   }
 end
 local luasnip = require'luasnip'
 local cmp = require 'cmp'
 local lspkind = require "lspkind"
 
 local t = function(str)
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
+   return vim.api.nvim_replace_termcodes(str, true, true, true)
 end
 
 _G.tab_complete = function()
-    if luasnip and luasnip.expand_or_jumpable() then
-        return t("<Plug>luasnip-expand-or-jump")
-    else
-        return t "<Tab>"
-      end
-  end
+   if luasnip and luasnip.expand_or_jumpable() then
+       return t("<Plug>luasnip-expand-or-jump")
+   else
+       return t "<Tab>"
+     end
+ end
 _G.s_tab_complete = function()
-    if luasnip and luasnip.jumpable(-1) then
-        return t("<Plug>luasnip-jump-prev")
-    else
-        return t "<S-Tab>"
-    end
+   if luasnip and luasnip.jumpable(-1) then
+       return t("<Plug>luasnip-jump-prev")
+   else
+       return t "<S-Tab>"
+   end
 end
 
 cmp.setup {
-    formatting = {
-        format = lspkind.cmp_format({
-            mode = 'symbol', -- show only symbol annotations
-            maxwidth = 75, 
-            before = function (entry, vim_item)
-              if entry.kind == "Keyword" then
-                return false
-              end
-              return vim_item
-              end
-        })
-    },
+   formatting = {
+       format = lspkind.cmp_format({
+           mode = 'symbol', -- show only symbol annotations
+           maxwidth = 75, 
+           before = function (entry, vim_item)
+             if entry.kind == "Keyword" then
+               return false
+             end
+             return vim_item
+             end
+       })
+   },
 
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
-    mapping = cmp.mapping.preset.insert({
-        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-j>'] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true,
-        },
-    }),
-    sources = {
-      { name = 'nvim_lsp', weight = 1 },
-      { name = 'luasnip', weight = 2 },
-    },
+   snippet = {
+       expand = function(args)
+           luasnip.lsp_expand(args.body)
+       end,
+   },
+   mapping = cmp.mapping.preset.insert({
+       ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+       ['<C-f>'] = cmp.mapping.scroll_docs(4),
+       ['<C-Space>'] = cmp.mapping.complete(),
+       ['<C-j>'] = cmp.mapping.confirm {
+           behavior = cmp.ConfirmBehavior.Replace,
+           select = true,
+       },
+   }),
+   sources = {
+     { name = 'nvim_lsp', weight = 1 },
+     { name = 'luasnip', weight = 2 },
+   },
 }
 
 require("luasnip.loaders.from_vscode").lazy_load()
